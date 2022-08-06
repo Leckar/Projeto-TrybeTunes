012d6aff68ee662751e93642160bcbca68e8dbe4
Leckar <gusleckar@gmail.com>
2022-08-06 16:43:52 -0300

    requisite 2 is broken

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
-import Album from '../pages/Album';
-import Favorites from '../pages/Favorites';
-import Login from '../pages/Login';
-import NotFound from '../pages/NotFound';
-import Profile from '../pages/Profile';
-import ProfileEdit from '../pages/ProfileEdit';
-import Search from '../pages/Search';
+import Album from './pages/Album';
+import Favorites from './pages/Favorites';
+import Login from './pages/Login';
+import NotFound from './pages/NotFound';
+import Profile from './pages/Profile';
+import ProfileEdit from './pages/ProfileEdit';
+import Search from './pages/Search';
 
 class App extends React.Component {
   render() {
---
+++ src/pages/Login.jsx
@@ -0,0 +1,70 @@
+import React, { Component } from 'react';
+import { Link, Redirect } from 'react-router-dom';
+import { createUser } from '../services/userAPI';
+import Loading from '../components/Loading';
+
+class Login extends Component {
+  constructor() {
+    super();
+    this.state = {
+      user: '',
+      isButtonDisabled: true,
+      loading: false,
+    };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.conditionalRender = this.conditionalRender.bind(this);
+  }
+
+  handleChange({ value }) {
+    const valid = 3;
+    return value.length >= valid ? this.setState({
+      user: value,
+      isButtonDisabled: false,
+    })
+      : this.setState({
+        user: value,
+        isButtonDisabled: true,
+      });
+  }
+
+  async handleSubmit() {
+    const { user } = this.state;
+    this.setState({ loading: true });
+    await createUser(user);
+    this.setState({ loading: false });
+  }
+
+  conditionalRender() {
+    const { user, isButtonDisabled, loading } = this.state;
+    const form = (
+      <div data-testid="page-login">
+        <form>
+          <input
+            type="text"
+            data-testid="login-name-input"
+            value={ user }
+            onChange={ ({ target }) => this.handleChange(target) }
+          />
+          <button
+            type="button"
+            data-testid="login-submit-button"
+            disabled={ isButtonDisabled }
+            onClick={ () => this.handleSubmit() }
+          >
+            Entrar
+          </button>
+        </form>
+      </div>
+    );
+    return loading ? <Loading /> : form;
+  }
+
+  render() {
+    return (
+      this.conditionalRender()
+    );
+  }
+}
+
+export default Login;
