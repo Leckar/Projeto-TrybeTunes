c4e99dc34b53d3041cd0b0de3653334df31dd999
Leckar <gusleckar@gmail.com>
2022-08-06 15:13:56 -0300

    requisite 1

---
+++ pages/NotFound.jsx
@@ -0,0 +1,12 @@
+// import PropTypes from 'prop-types';
+import React, { Component } from 'react';
+
+class NotFound extends Component {
+  render() {
+    return (
+      <div data-testid="page-not-found">404: PAGE NOT FOUND</div>
+    );
+  }
+}
+
+export default NotFound;
---
+++ pages/Search.jsx
@@ -0,0 +1,12 @@
+// import PropTypes from 'prop-types';
+import React, { Component } from 'react';
+
+class Search extends Component {
+  render() {
+    return (
+      <div data-testid="page-search">Search</div>
+    );
+  }
+}
+
+export default Search;
